2082fb1246104930732154f9907e9e06748f4588
j-renan <desen.renan@gmail.com>
2022-02-20 15:33:31 -0300

    corrigindo parametros

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const appName = 'my-links';
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + `/dist/angular/${appName}`));
+app.use(express.static(__dirname + `/dist/${appName}`));
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname, `dist/angular/${appName}/index.html`)
+    res.sendFile(__dirname, `dist/${appName}/index.html`)
 });
 
 app.listen(PORT, () => {
